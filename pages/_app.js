f693107653362d7cf6919a57bb7e59a5b7519fcf
Diogo Alves <diogo.alves.ti@gmail.com>
2021-07-14 12:05:49 -0300

    Aula 01

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,32 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { AlurakutStyles } from '../src/lib/AlurakutCommons';
 
 const GlobalStyle = createGlobalStyle`
-  body {
+  * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
   }
-`
+
+  body {
+    font-family: sans-serif;
+    background-color: #D9E6F6;
+  }
+
+  #__next {
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+  }
+
+  img {
+    display: block;
+    height: auto;
+    max-width: 100%;
+  }
+
+  ${AlurakutStyles}
+`;
 
 const theme = {
   colors: {
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,66 @@
-import styled from 'styled-components'
+import MainGrid from '../src/components/MainGrid';
+import Box from '../src/components/Box';
+import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations';
+import { AlurakutMenu, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons';
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+function ProfileSideBar(props) {
+  return (
+    <Box>
+      <img src={`https://github.com/${props.githubUser.username}.png`} style={{ borderRadius: '8px' }} />
+    </Box>
+  )
+}
 
 export default function Home() {
-  return <Title>My page</Title>
+  const githubUser = {
+    username: 'diogo-alves',
+    name: 'Diogo Alves'
+  };
+  const aluraPeople = [
+  	'juunegreiros',
+  	'omariosouto',
+  	'peas',
+  	'rafaballerini',
+  	'marcobrunodev',
+  	'felipefialho'
+  ];
+  return (
+    <>
+      <AlurakutMenu githubUser={githubUser.username} />
+      <MainGrid>
+        <div className="profileArea" style={{ gridArea: 'profileArea' }}>
+          <ProfileSideBar githubUser={githubUser} />
+        </div>
+        <div className="welcomeArea" style={{ gridArea: 'welcomeArea' }}>
+          <Box>
+            <h1 className="title">Bem vindo, {githubUser.name}</h1>
+            <OrkutNostalgicIconSet />
+          </Box>
+        </div>
+        <div className="profileRelationsArea" style={{ gridArea: 'profileRelationsArea' }}>
+          <ProfileRelationsBoxWrapper>
+            <h2 className="smallTitle">
+              Pessoas da Comunidade <a href="#" className="boxLink">({aluraPeople.length})</a>
+            </h2>
+            <ul>
+              {aluraPeople.map(person => {
+                return (
+                  <li key={person}>
+                    <a href={`/users/${person}`}>
+                      <img src={`https://github.com/${person}.png`} style={{ borderRadius: '8px' }} />
+                      <span>{person}</span>
+                    </a>
+                  </li>
+                )
+              })}
+            </ul>
+            <div>
+              <hr />
+              <a href="" className="boxLink">Ver todas</a>
+            </div>
+          </ProfileRelationsBoxWrapper>
+        </div>
+      </MainGrid>
+    </>
+  )
 }
